d26c5fd669cc78a071863def7806edb07ed1447f
IanN <ian.neilsen@gmail.com>
2011-05-30 14:30:25 +1000

    files and folders uploaded - inital setup with mongo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Studentresults::Application.config.secret_token = '26d86b15a27ecb5a466bcb690bb0c618e81bbb93bd870c68a5a5c8627a1e1a5d8ff8bbef61cfa511f0422c905239cd168b9770d76f4a4c118bf6b37aafdb99ce'
